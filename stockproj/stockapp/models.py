5c9dcf191cb8e4130470005df7b31de3b6e49814
madhu kiran <117820682+bmk25@users.noreply.github.com>
2023-05-31 16:19:01 +0530

    Delete stockproj directory

--- stockproj/stockapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class stock(models.Model):
-    sno =models.IntegerField()
-    arriveddate =models.DateField()
-    productName= models.CharField(max_length=20 )
-    productQ =models.CharField(max_length=5)
-    productP= models.FloatField()
